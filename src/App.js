f42ea204d8b84ed922889410b3105fee1a62474c
DmitriyDavydovSuki <93195604+DmitriyDavydovSuki@users.noreply.github.com>
2022-07-15 15:03:55 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import { useState } from "react"
 import './App.css';
 
 function App() {
+
+  const [value, setValue] = useState("")
+  const [list, setList] = useState([])
+
+  const addTodo = () => {
+    const newTodo = value;
+    setValue("")
+    setList([...list, newTodo])
+  }
+
+  const deleteItem = (index) => {
+    const newList = list.filter((e, i) => i !== index)
+    setList(newList)
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <input value={value} onChange={e => setValue(e.target.value)} />
+        <button onClick={addTodo}>Add todo</button>
+        {list.map((el, index) =>
+          <div className="list-item" key={index}>
+            {index} - {el}
+            <button className="delete-btn" onClick={() => deleteItem(index)}>x</button>
+          </div>)
+        }
+      </header >
+    </div >
   );
 }
 
